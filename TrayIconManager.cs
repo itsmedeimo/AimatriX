956c6733c0d22189ebf8217450dfed1f736c98d3
Deimo <itsmedeimo@gmail.com>
2025-04-23 01:32:16 -0700

    Custom Crosshair Support
    Users now can select their own .png crosshairs

--- TrayIconManager.cs
+++ TrayIconManager.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Drawing;
+using System.IO;
 using System.Windows.Forms;
 
 namespace AimatriX
@@ -7,27 +8,58 @@ namespace AimatriX
     public class TrayIconManager : IDisposable
     {
         private NotifyIcon trayIcon;
+        private CrosshairForm crosshairForm;
 
-        public TrayIconManager()
+        public TrayIconManager(CrosshairForm form)
         {
-            trayIcon = new NotifyIcon
-            {
-                Icon = new Icon("Resources/aimatrix.ico"),
-                Visible = true,
-                Text = "AimatriX"
-            };
+            crosshairForm = form;
+
+            trayIcon = new NotifyIcon();
+            trayIcon.Icon = new Icon("Resources/aimatrix.ico");
+            trayIcon.Visible = true;
 
             var contextMenu = new ContextMenuStrip();
-            var exitItem = new ToolStripMenuItem("Exit");
-            exitItem.Click += (s, e) => Application.Exit();
-            contextMenu.Items.Add(exitItem);
+            contextMenu.Items.Add("Select Crosshair", null, SelectCrosshair);
+            contextMenu.Items.Add("Center Crosshair", null, CenterCrosshair);
+            contextMenu.Items.Add("Exit", null, Exit);
+
             trayIcon.ContextMenuStrip = contextMenu;
+        }
+
+        private void SelectCrosshair(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "PNG files (*.png)|*.png";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string selectedFile = openFileDialog.FileName;
+                    if (File.Exists(selectedFile))
+                    {
+                        crosshairForm.UpdateCrosshairImage(selectedFile);
+                    }
+                }
+            }
+        }
+
+        private void CenterCrosshair(object sender, EventArgs e)
+        {
+            crosshairForm.CenterCrosshair();
+        }
+
+        private void Exit(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
 
         public void Dispose()
         {
-            trayIcon.Visible = false;
-            trayIcon.Dispose();
+            if (trayIcon != null)
+            {
+                trayIcon.Visible = false;
+                trayIcon.Dispose();
+                trayIcon = null;
+            }
         }
     }
 }
